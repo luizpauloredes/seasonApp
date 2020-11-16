c71171238f508c5cba37a6681b0ef77d5dc9c436
unknown <luizpaulo.redes@gmail.com>
2020-11-16 14:17:11 -0300

    appy working with  geolocation

--- src/index.jsx
+++ src/index.jsx
@@ -1,33 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import SeasonDisplay from './SeasonDisplay';
 
 
 class App extends React.Component {
+   
 
-    constructor(props) {
-        super(props);
+    state= { lat: null, errorMessage:''};
 
-        this.state = { lat: null, errorMessage: '' };
+    componentDidMount() {
 
         window.navigator.geolocation.getCurrentPosition(
-
-
-
-            position => {
-
-                //we called setstate!!
-                this.setState({ lat: position.coords.latitude })
-            },
-            err => {
-                this.setState({ errorMessage: err.message })
-            }
-
+            position => this.setState({ lat: position.coords.latitude }) ,
+            err =>  this.setState({ errorMessage: err.message })            
         );
-
-    }
-
-
+    } 
+    
     //React says we have to define render!!
     render() {
 
@@ -42,14 +30,7 @@ class App extends React.Component {
         }
         
         if  (!this.state.errorMessage &&  this.state.lat ){
-            return (
-
-                <div>
-                    Latitude: {this.state.lat}<br />                
-                             
-                </div>
-    
-            )
+            return <SeasonDisplay lat={this.state.lat}/>
         }
 
         return <div>Loading !</div>;
